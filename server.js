c27d0db242e50b9496d2948cc81463e579676fda
Pedro Henrique <pedrohenr1qqstudios@gmail.com>
2024-10-22 11:05:33 -0300

    Fix: Add internal documentation

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
+// Intenal require
 const app = require('./index');
 
+// Port at server will run
 const port = 3000;
 
+// Starting server
 app.listen(port, ()=>{
   console.log(`Server running at http://localhost:${port}`);
 })
