7ec5e1bffe3067a04ca629af45ce5ec9cf014bb0
Pedro Henrique <pedrohenr1qqstudios@gmail.com>
2024-10-24 21:32:43 -0300

    Feat: add Product Validate middleware

---
+++ src/middleware/product.middleware.js
@@ -0,0 +1,30 @@
+function showErrors(errors){
+  if(errors.length > 1) return `The fields '${errors}' are empties`;
+  else return `The field '${errors}' is empty`
+}
+
+const validateProduct = async (req, res, next) => {
+  let errors = []
+  const product = req.body;
+  const productCategory =  req.body.category;
+
+  if(!product.name) errors.push("name ");
+  if(!product.description) errors.push("description ");
+  if(!product.size) errors.push("size ");
+  if(!product.unitPrice) errors.push("unitPrice ");
+  if(!product.image) errors.push("image ");
+  if(!productCategory) errors.push("category");
+
+  if(productCategory != undefined) {
+    if(!productCategory._id) errors.push("category ID");
+  }
+
+  if(errors.length != 0){
+    return res.status(400).send({message: showErrors(errors)});
+  }
+  return next();
+}
+
+module.exports = {
+  validateProduct
+}
--- src/router/product.router.js
+++ src/router/product.router.js
@@ -4,6 +4,7 @@ const router = require('express').Router();
 // Internal Requires
 const productController = require('../controller/product.controller');
 const authMiddleware = require('../middleware/auth.middleware');
+const {validateProduct} = require('../middleware/product.middleware');
 
 // Create routes
 // GET
@@ -11,10 +12,10 @@ router.get('/findById/:id', authMiddleware, productController.findProductById);
 router.get('/findAll', authMiddleware, productController.findAllProducts);
 
 // POST
-router.post('/create', authMiddleware, productController.createProduct);
+router.post('/create', authMiddleware, validateProduct, productController.createProduct);
 
 // PUT
-router.put('/update/:id', authMiddleware, productController.updateProduct);
+router.put('/update/:id', authMiddleware, validateProduct, productController.updateProduct);
 
 // DELETE
 router.delete('/delete/:id', authMiddleware, productController.deleteProduct);
