141af361270ee2bf16df86f761cb4c6db69bda25
Pedro Henrique <pedrohenr1qqstudios@gmail.com>
2024-10-25 23:02:38 -0300

    Docs: Add comments in docs.router file

--- src/router/docs.router.js
+++ src/router/docs.router.js
@@ -1,10 +1,14 @@
+// Dependenceis
 const router = require("express").Router();
-
 const swaggerUi = require('swagger-ui-express');
+
+// Internal requires
 const swaggerDocument = require('../../swagger.json');
 
+// Create the route to swagger document
 router.use('/api-routes', swaggerUi.serve);
 
+// Link and setup the swagger document in the route '/docs/api-routes'
 router.get('/api-routes', swaggerUi.setup(swaggerDocument));
 
 module.exports = router;
